8bf69f145e016b1af0a8c0d02a7607a037637b3d
klswkj <klswkj@gmail.com>
2021-01-15 14:52:57 +0900

    111

---
+++ Graphics/TechniqueWindow.h
@@ -0,0 +1,41 @@
+#pragma once
+
+class Technique;
+
+class ITechniqueWindow
+{
+public:
+	// TODO: add callback for visiting each mesh
+	virtual ~ITechniqueWindow()
+	{
+	}
+	void SetTechnique(Technique* pTech_in)
+	{
+		m_pTechnique = pTech_in;
+		m_TechniqueIndex++;
+		OnSetTechnique();
+	}
+	void SetStep(Step* pStep_in)
+	{
+		m_pStep = pStep_in;
+		m_StepIndex++;
+		OnSetStep();
+	}
+	bool VisitBuffer(/*Dcb::Buffer& buf*/)
+	{
+		m_BufferIndex++;
+		return false;
+	}
+protected:
+	virtual void OnSetTechnique() = 0;
+	virtual void OnSetStep() = 0;
+	virtual bool OnVisitBuffer(/*Dcb::Buffer&*/) = 0;
+
+protected:
+	Step* m_pStep           { nullptr };
+	Technique* m_pTechnique { nullptr };
+
+	size_t m_TechniqueIndex = -1;
+	size_t m_StepIndex      = -1;
+	size_t m_BufferIndex    = -1;
+};
